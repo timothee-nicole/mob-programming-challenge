0f094f65b53573f032dbf4e0dcf02ecb89432599
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-17 10:38:47 +0200

    iteration 5 done

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-require("dotenv").config()
-require("./config/mongodb.js")
+require("dotenv").config();
+require("./config/mongodb.js");
 
-console.log("Hello World")
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+console.log("Hello World");
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
+const users = [
+  {
+    name: "Toto",
+    email: "toto@mail.com",
+    favoriteLanguage: "Javascript",
+  },
+];
+
+const images = ["img/coding1.jpg", "img/coding2.jpg", "img/coding3.jpg"];
+
+app.listen(process.env.PORT, () => {
+  console.log(`server ready to rock at http://localhost:${process.env.PORT}`);
+});
